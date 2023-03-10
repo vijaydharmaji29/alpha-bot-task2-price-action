465b54ce32b4ed5b86b79fe08b5e347b3b75cc60
vijaydharmaji29 <vijaydharmaji@outlook.com>
2023-03-11 02:01:53 +0530

    intraday

--- brain.py
+++ brain.py
@@ -12,6 +12,17 @@ def calculate(session_data, positions, capital): #list of all active positions, 
     execute = []
     slrl_limit = 20
 
+    #squaring off at the end of the day - intraday - makes it worseoff though
+    if session_data.time == '15:29:00' and len(positions) > 0:
+        if positions[0].trade_type == True: #if i am on a long position, squaring the long position off by selling
+            new_action = action(session_data.symbol, positions[0].qty, False, True, session_data.close, session_data.datetime, True)
+            execute.append(new_action)
+        else: #if i am on a short position, squaring the short position off by buying
+            new_action = action(session_data.symbol, positions[0].qty, True, False, session_data.close, session_data.datetime, False)
+            execute.append(new_action)
+
+        return execute
+
     buy_signal = (session_data.close > session_data.filt) and (session_data.supports - slrl_limit < session_data.close) and (session_data.supports + slrl_limit > session_data.close)
     sell_signal = (session_data.close < session_data.filt) and (session_data.direction == -1) and (session_data.resistances - slrl_limit < session_data.close) and (session_data.resistances + slrl_limit > session_data.close)
     
@@ -21,9 +32,20 @@ def calculate(session_data, positions, capital): #list of all active positions, 
         new_action = action(session_data.symbol, qty, True, False, session_data.close, session_data.datetime, True)
         execute.append(new_action)
     
-    elif sell_signal and (len(positions) == 1):
+    elif sell_signal and (len(positions) == 1) and positions[0].trade_type == True:
         qty = positions[0].qty
         new_action = action(session_data.symbol, qty, False, True, session_data.close, session_data.datetime, True)
+        execute.append(new_action)
+
+    #for shorting stocks
+    elif buy_signal and len(positions) == 1 and positions[0].trade_type == False:
+        qty = positions[0].qty
+        new_action = action(session_data.symbol, qty, True, False, session_data.close, session_data.datetime, False)
+        execute.append(new_action)
+
+    elif sell_signal and len(positions) == 0:
+        qty = int(capital/session_data.close)
+        new_action = action(session_data.symbol, qty, False, True, session_data.close, session_data.datetime, False)
         execute.append(new_action)
     
     return execute
